b7b374b89bfcb92ce207545098b0e43153dbdb53
JuanesLinux <juanesph@gmail.com>
2020-04-19 16:59:40 -0400

    tratando arreglar error al crear

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
     validates :title, presence: true
     validates :image_url, presence: true
     validates :content, presence: true
+    
     before_save  :erase_spoiler
 
     def erase_spoiler
